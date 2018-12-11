6f8ad9861e6cf0c689d1883642fb5f04455aa11c
lioartoil <a.chanakant@gmail.com>
2018-12-11 18:57:14 +0700

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,46 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect(
+	'mongodb://localhost:27017/TodoApp',
+	{ useNewUrlParser: true },
+	(err, client) => {
+		if (err) {
+			return console.log('Unable to connect to MongoDB server');
+		}
+		console.log('Connected to MongoDB server');
+		const db = client.db('TodoApp');
+
+		// db.collection('Todos').insertOne(
+		// 	{
+		// 		text: 'Something to do',
+		// 		completed: false
+		// 	},
+		// 	(err, result) => {
+		// 		if (err) {
+		// 			return console.log('Unable to insert todo', err);
+		// 		}
+
+		// 		console.log(JSON.stringify(result.ops, undefined, 2));
+		// 	}
+		// );
+
+		// Insert new doc into Users (name, age, location)
+		// db.collection('Users').insertOne(
+		// 	{
+		// 		name: 'Chanakan',
+		// 		age: 27,
+		// 		location: 'Pathum'
+		// 	},
+		// 	(err, result) => {
+		// 		if (err) {
+		// 			return console.log('Unable to insert users', err);
+		// 		}
+
+		// 		console.log(result.ops[0]._id.getTimestamp());
+		// 	}
+		// );
+
+		client.close();
+	}
+);
