2a13d9e518d32f8296d099ff8fe354cb31e4c73f
lioartoil <a.chanakant@gmail.com>
2018-12-12 16:32:59 +0700

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(
-	'mongodb://localhost:27017/TodoApp',
+	process.env.MONGODB_URI ||
+		'mongodb://heroku_lrws61mq:3cuspl83sfd5qtrvrrlch7quog@ds251240.mlab.com:51240/heroku_lrws61mq',
 	{ useNewUrlParser: true }
 );
 
