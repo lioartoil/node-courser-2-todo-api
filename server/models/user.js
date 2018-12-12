9353cba2fffd0609b1715b97ad42856dbdd7ef3c
lioartoil <a.chanakant@gmail.com>
2018-12-12 20:41:27 +0700

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -52,8 +52,23 @@ UserSchema.methods.generateAuthToken = function() {
 
 	user.tokens = user.tokens.concat([{ access, token }]);
 
-	return user.save().then(() => {
-		return token;
+	return user.save().then(() => token);
+};
+
+UserSchema.statics.findByToken = function(token) {
+	const User = this;
+	let decoded;
+
+	try {
+		decoded = jwt.verify(token, 'abc123');
+	} catch (e) {
+		return Promise.reject();
+	}
+
+	return User.findOne({
+		_id: decoded._id,
+		'tokens.token': token,
+		'tokens.access': 'auth'
 	});
 };
 
